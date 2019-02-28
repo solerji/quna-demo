3a08c124b06a203dd16377eb9bc3a2444a43d0f5
soloerji <2474499153@qq.com>
2019-02-28 11:42:32 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false;
-
+//解决点击事件延迟
+fastClick.attach(document.body)
 new Vue({
   router,
   store,
