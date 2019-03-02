32388fd7ee0a6bea5bcd9fd66b82f510af235901
soloerji <2474499153@qq.com>
2019-03-02 21:07:40 +0800

    首页轮播图

---
+++ src/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="warpper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1801/93/ce59d182aca07102.jpg_640x200_ba03d44c.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// 样式穿透
+// .wrapper >>> .swiper-pagination-bullet-active
+// background: #fff
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  background: #eee
+
+.swiper-img
+  width: 100%
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 //解决点击事件延迟
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
