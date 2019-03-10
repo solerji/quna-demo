5b37830432e0ec0f68b5066e00bf07ecbb54a0f8
soloerji <2474499153@qq.com>
2019-03-10 11:29:41 +0800

    城市选择模块

--- src/router.js
+++ src/router.js
@@ -1,16 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import City from './views/city/City.vue'
 
-
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  routes: [{
-      path: "/",
-      name: "home",
+  routes: [
+    {
+      path: '/',
+      name: 'home',
       component: Home
     },
-
+    {
+      path: '/city',
+      name: 'City',
+      component: City
+    }
   ]
-});
+})
---
+++ src/views/city/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+
+.header {
+  overflow: hidden
+  height: $headerHeight
+  line-height: $headerHeight
+  text-align: center
+  color: #fff
+  background: $bgColor
+  font-size: 0.32rem
+
+  .header-back {
+    position: absolute
+    width: 0.64rem
+    top: 0
+    left: 0
+    text-align: center
+    font-size: 0.4rem
+    color: #fff
+  }
+}
+</style>
